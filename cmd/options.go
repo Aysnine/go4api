13106a50ea6e5ac19fc28991d49622f1f54f8d97
CNine <cnine229@gmail.com>
2022-08-27 22:41:11 +0800

    chore(release): v0.71.8-15

--- cmd/options.go
+++ cmd/options.go
@@ -163,7 +163,7 @@ func init() {
 func usage() {
 	fmt.Fprintf(os.Stderr, `
 -----
-go4api version: 0.71.8-13
+go4api version: 0.71.8-15
 
 forked: github.com/Aysnine/go4api
 
--- db/sqldb/db_mysql.go
+++ db/sqldb/db_mysql.go
@@ -26,6 +26,8 @@ func InitMySqlConnection() map[string]*sql.DB {
 
 	dbs := cmd.GetDbConfig()
 
+	fmt.Println("dbs: ", dbs)
+
 	for k, v := range dbs {
 		envMap := utils.GetOsEnviron()
 
@@ -38,6 +40,9 @@ func InitMySqlConnection() map[string]*sql.DB {
 		defaultSchema := ""
 
 		conInfo := user + ":" + password + "@tcp(" + ip + ":" + port + ")/" + defaultSchema
+
+		fmt.Println("before open: ", k, ip, port, user, password, conInfo)
+
 		db, _ := sql.Open("mysql", conInfo)
 		db.SetMaxOpenConns(2000)
 		db.SetMaxIdleConns(1000)
@@ -49,6 +54,10 @@ func InitMySqlConnection() map[string]*sql.DB {
 		}
 
 		dbIndicator := strings.ToLower(k)
+
+		fmt.Println("after open: dbIndicator", dbIndicator)
+		fmt.Println("after open: ping err", err)
+
 		sqlCons[dbIndicator] = db
 	}
 
